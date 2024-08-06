585681de93aad8611d89d419c0c28340e5515ad3
Papita <papita@Jozsefs-MacBook-Pro.local>
2024-08-06 16:25:31 +0200

    jQuery practice

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,4 +1,28 @@
+$("h1").addClass("big-title"); 
 
-$(document).ready(function(){
-    $("h1").css("color", "red");
+$("h1").text("Bye"); 
+
+$("button").html("<em>Hey</em>");
+
+$('a').attr("href","https://www.yahoo.com/" ); 
+
+$("button").click(function() {
+    $("h1").css("color", "purple");
 });
+
+$("document").keypress(function(event){
+    $("h1").text("event.key"); 
+});
+
+//$("h1").on("mouseover", function(){
+    //$("h1").css("color", "purple"); 
+//}); 
+
+//$("h1").on("click", function(){
+    //$("h1").animate({opacity: 0.5});
+//}); 
+
+$("h1").on("click", function(){
+    $("h1").slideUp().slideDown().animate({opacity: 0.5});
+}); 
+
